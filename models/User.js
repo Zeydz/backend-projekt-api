de4a9334627bdba9c1b449f0178c0475f2fb5797
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-05-09 21:00:54 +0200

    Skapat fungerande inloggning via API

--- models/User.js
+++ models/User.js
@@ -1,12 +1,22 @@
 /* Schema för att skapa en användare i databasen */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
 /* Schema */
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     created: { type: Date, default: Date.now}
 });
+
+/* Jämför lösenord */
+userSchema.methods.comparePassword = async function(password) {
+    try {
+        return await bcrypt.compare(password, this.password);
+    } catch (error) {
+        throw error
+    }
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,7 @@ router.post('/create-admin', async (req, res) => {
             return res.status(400).json({ message: 'Fel inmatning, skicka användarnamn och lösenord'})
         }
         
-        /* Skapa användare */
+        /* Skapa användare med hashat lösenord */
         const hashedPassword = await bcrypt.hash(password, 10);
         const adminUser = new User ({
             username: username,
@@ -42,5 +42,35 @@ router.post('/create-admin', async (req, res) => {
         res.status(500).json({ message: 'Det uppstod ett fel' + error});
     }
 });
+
+/* Logga in användare, skicka tillbaka token */
+router.post('/login', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        /* Validering */
+        if(!username || !password) {
+            return res.status(400).json({ message: 'Fel inmatning, skicka användarnamn och lösenord.'})
+        }
+
+        /* Titta efter användarnamnet */
+        const user = await User.findOne({username});
+        if (!user) {
+            return res.status(401).json({ message:'Fel användarnamn eller lösenord.'})
+        }
+
+        /* Kontrollera lösenord */
+        const isPasswordMatch = await user.comparePassword(password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ message: 'Fel användarnamn/lösenord.'})
+        } else {
+            const payload = { username: username};
+            const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1h'});
+            const response = { message: 'Användare inloggad', token: token}
+            res.status(200).json({ response });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error: ' + error})
+    }
+})
 
 module.exports = router;
