7faa7bb429c42dce1a86f9efeb83bdd59ae4d89e
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-05-11 15:11:11 +0200

    Lagt till möjlighet att ta bort meny

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -27,4 +27,15 @@ router.post('/menu', async (req, res) => {
     }
 });
 
+/* Ta bort en meny */
+router.delete('/menu/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deletedMenu = await MenuItem.findByIdAndDelete(id);
+        res.json({ message: 'Meny borttagen', deletedMenu });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
