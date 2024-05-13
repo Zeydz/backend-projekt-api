ac464b36b28e870b2bcaec09e9d48e0a7176507a
Jocke <joakim03moller@gmail.com>
2024-05-13 13:19:57 +0200

    Lagt till möjlighet att redigera meny

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -38,4 +38,16 @@ router.delete('/menu/:id', async (req, res) => {
     }
 });
 
+router.put('/menu/:id',  async (req, res) => {
+    const id = req.params.id;
+    const { itemName, description, price } = req.body;
+    
+    try {
+        const updatedMenu = await MenuItem.findByIdAndUpdate(id, { itemName, description, price }, { new: true});
+        res.status(200).json('Menyn uppdaterad: ' + updatedMenu);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
+})
+
 module.exports = router;
